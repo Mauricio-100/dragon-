fcb47f8cfa08d437987a3f43527a5724b8b1a803
Mauricio-100 <ceoseshell@gmail.com>
2025-09-29 13:20:20 +0100

    Update search.js

--- engine/search.js
+++ engine/search.js
@@ -1,11 +1,12 @@
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 
 // Exemple moteur gratuit : DuckDuckGo API
 async function searchQuery(query) {
   const url = `https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json`;
   const res = await fetch(url);
   const data = await res.json();
-  return data.RelatedTopics.slice(0,3); // renvoie 3 premiers résultats
+
+  return data.RelatedTopics.slice(0, 3); // renvoie les 3 premiers résultats
 }
 
-module.exports = { searchQuery };
+export { searchQuery };

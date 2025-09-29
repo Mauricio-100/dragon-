10cec8ada2f756c8facd057f95afd1567c97511e
Mauricio-100 <ceoseshell@gmail.com>
2025-09-29 13:18:35 +0100

    Update pense.js

--- neraune/pense.js
+++ neraune/pense.js
@@ -1,16 +1,22 @@
-const brain = require('brain.js');
-const { recall } = require('../AprentisSage/know');
+import brain from "brain.js";
+import { recall } from "../AprentisSage/know.js";
 
 const net = new brain.NeuralNetwork();
 
 function pense(input) {
-  // Convertir le texte en input simplifié
+  // Récupère les données apprises
   const trainedData = recall();
-  net.train(trainedData); // on peut améliorer plus tard pour entraîner progressivement
 
+  // Entraînement du réseau
+  net.train(trainedData);
+
+  // Test avec un input simplifié
   const result = net.run({ [input]: 1 });
-  if (result.create) return "exec:echo 'Création fichier...'";
+
+  if (result.create) {
+    return "exec:echo 'Création fichier...'";
+  }
   return "Je ne sais pas encore quoi faire...";
 }
 
-module.exports = { pense };
+export { pense };
